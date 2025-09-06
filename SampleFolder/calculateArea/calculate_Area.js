623a7caa92c71e0976adae8ef64bc6b7ff22abb9
gabriel22l <agabrielguzman30@gmail.com>
2025-09-06 12:28:11 -0400

    calc total

--- SampleFolder/calculateArea/calculate_Area.js
+++ SampleFolder/calculateArea/calculate_Area.js
@@ -8,3 +8,11 @@ function calculateArea(){
     document.getElementById('result').innerText = `The area of the rectangle is ${area}`;
 
 }
+let total = 0;
+function calculateTotal(){
+    let groceries = document.getElementsByClassName('grocery');
+    for(grocery of groceries){
+        total += parseFloat(grocery.value);
+    }
+    document.getElementById('Total').innerText = `The total amount is: $${total}`
+}
